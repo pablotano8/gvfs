0a3c485507d49e925e7ef51178c8f5b84f5e6280
csmfindling <charles.findling@gmail.com>
2021-09-20 18:52:54 +0200

    made a function static

--- policy_networks/net.py
+++ policy_networks/net.py
@@ -1,12 +1,20 @@
 from abc import ABC
-
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
 import numpy as np
 
 
+def reward_to_go(rewards):
+    n = len(rewards)
+    rtgs = np.zeros_like(rewards)
+    for i in reversed(range(n)):
+        rtgs[i] = rewards[i] + (rtgs[i + 1] if i + 1 < n else 0)
+    return rtgs
+
+
 class Net(nn.Module, ABC):
-    def __init__(self, input_shape, n_actions):
+
+    def __init__(self):
         super(nn.Module, self).__init__()
 
     # make function to compute action distribution
@@ -22,10 +30,3 @@ class Net(nn.Module, ABC):
     def compute_loss(self, obs, act, weights):
         logp = self.get_policy(obs).log_prob(act)
         return -(logp * weights).mean()
-
-    # compute rewards to go
-    def reward_to_go(self, rewards):
-        n = len(rewards)
-        self.rtgs = np.zeros_like(rewards)
-        for i in reversed(range(n)):
-            self.rtgs[i] = rewards[i] + (rtgs[i + 1] if i + 1 < n else 0)
