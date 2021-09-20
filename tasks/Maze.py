63858e7963835c38be8721777b4e27fde72b6f27
csmfindling <charles.findling@gmail.com>
2021-09-20 13:45:13 +0200

    took out obsolet

--- tasks/Maze.py
+++ tasks/Maze.py
@@ -14,7 +14,6 @@ class Maze(BaseMaze):
         return Maze_config.x.shape
 
     def make_objects(self):
-        global x
         free = Object('free', 0, Color.free, False, np.stack(np.where(Maze_config.x == 0), axis=1))
         obstacle = Object('obstacle', 1, Color.obstacle, True, np.stack(np.where(Maze_config.x == 1), axis=1))
         agent = Object('agent', 2, Color.agent, False, [])
