486e7c914477e8b6677af9f1cae6af26c70e9fd7
csmfindling <charles.findling@gmail.com>
2021-09-22 13:55:09 +0200

    added comment

--- tasks/RandomMaze.py
+++ tasks/RandomMaze.py
@@ -6,8 +6,9 @@ from tasks import Maze_config
 from mazelab.generators import random_maze
 
 
-def main(start_position: str, env_name: str = 'RandomMaze-v0', size: int = 20) -> object:
+def main(start_position: str = 'goal_position', env_name: str = 'RandomMaze-v0', size: int = 20) -> object:
     if start_position not in ['goal_position']:
+        print('For the moment, when a new maze is initialized, the agent is placed at the reward')
         raise NotImplementedError('This is not implemented yet')
     gym.envs.register(id=env_name, entry_point=Env, max_episode_steps=200)
     Maze_config.x = random_maze(width=size, height=size, complexity=1, density=0.5)
