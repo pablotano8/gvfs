69d80d910793fbded6528e637e8e5327a1968ee4
csmfindling <charles.findling@gmail.com>
2021-09-24 15:38:34 +0200

    gpu compatible

--- policy_networks/net.py
+++ policy_networks/net.py
@@ -26,13 +26,13 @@ class Net(nn.Module, ABC):
         return -(logp * weights).mean()
 
     # get policy for all states
-    def get_policy_all_states(self, env, gvf_net) -> np.ndarray:
+    def get_policy_all_states(self, env, gvf_net, device) -> np.ndarray:
         size = gvf_net.size
         policy_all_states = np.zeros([size, size, gvf_net.num_actions])
         for x_pos in range(size):
             for y_pos in range(size):
                 pos = np.array([x_pos, y_pos])
                 r_pos = np.squeeze(env.maze.objects.goal.positions).copy()
-                obs = torch.as_tensor(format_input(pos, r_pos, size, gvf_net), dtype=torch.float)
-                policy_all_states[x_pos, y_pos] = self.get_policy(obs).probs.detach().numpy()
+                obs = torch.as_tensor(format_input(pos, r_pos, size, gvf_net), dtype=torch.float).to(device)
+                policy_all_states[x_pos, y_pos] = self.get_policy(obs).probs.cpu().detach().numpy()
         return policy_all_states
