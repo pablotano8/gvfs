dc2b83b492422d175f127190ddbd5effbb948f93
pablotanoretamales <pablotanoretamales@gmail.com>
2021-11-03 13:17:56 +0100

    update get_policy_all_states to include subgoals

--- policy_networks/net.py
+++ policy_networks/net.py
@@ -28,14 +28,19 @@ class Net(nn.Module, ABC):
         return -(logp * weights).mean()
 
     # get policy for all states
-    def get_policy_all_states(self, env, gvf_net, device) -> np.ndarray:
+    def get_policy_all_states(self, env, gvf_net, which_subgoal, device) -> np.ndarray:
         size = gvf_net.size
         policy_all_states = np.zeros([size, size, gvf_net.num_actions])
         h = torch.zeros(self.hidden_dim, dtype=torch.float32).to(device) if self.name == 'rnn' else None
         for x_pos in range(size):
             for y_pos in range(size):
                 pos = np.array([x_pos, y_pos])
-                r_pos = np.squeeze(env.maze.objects.goal.positions).copy()
+                if which_subgoal==0:
+                    r_pos = np.squeeze(env.maze.objects.k1.positions).copy()
+                elif which_subgoal==1:
+                    r_pos = np.squeeze(env.maze.objects.k2.positions).copy()
+                elif which_subgoal==2:
+                    r_pos = np.squeeze(env.maze.objects.xf.positions).copy()
                 obs = torch.as_tensor(format_input(pos, r_pos, size, gvf_net), dtype=torch.float).to(device)
                 categorical, _ = self.get_policy(obs, h)
                 policy_all_states[x_pos, y_pos] = categorical.probs.cpu().detach().numpy()
