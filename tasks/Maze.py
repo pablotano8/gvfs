88d8509859c2bba3bd541dfec66d78209b1f38e8
pablotanoretamales <pablotanoretamales@gmail.com>
2021-11-03 13:19:36 +0100

    Update Maze.py

--- tasks/Maze.py
+++ tasks/Maze.py
@@ -15,8 +15,11 @@ class Maze(BaseMaze):
         return Maze_config.x.shape
 
     def make_objects(self):
+        k1 = Object('k1', 3, [0.2,0.5,0.8], False, [])
+        k2 = Object('k2', 4, [0.8,0.5,0.2],  False, [])
+        xf = Object('xf', 5, Color.goal ,False, [])
         free = Object('free', 0, Color.free, False, np.stack(np.where(Maze_config.x == 0), axis=1))
         obstacle = Object('obstacle', 1, Color.obstacle, True, np.stack(np.where(Maze_config.x == 1), axis=1))
         agent = Object('agent', 2, Color.agent, False, [])
-        goal = Object('goal', 3, Color.goal, False, [])
-        return free, obstacle, agent, goal
+        
+        return free, obstacle, agent, k1, k2, xf
